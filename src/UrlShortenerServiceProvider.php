d2a47085eab3f4b88b90ed29f5740b5c152629d0
whoami213 <whoami213@users.noreply.github.com>
2021-09-28 03:16:37 +0000

    Fix styling

--- src/UrlShortenerServiceProvider.php
+++ src/UrlShortenerServiceProvider.php
@@ -4,7 +4,6 @@ namespace Marjose\UrlShortener;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Marjose\UrlShortener\Commands\UrlShortenerCommand;
 
 class UrlShortenerServiceProvider extends PackageServiceProvider
 {
