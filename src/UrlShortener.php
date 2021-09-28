b35f6627331b9a86bb4ebaf93c38ec2efde6888e
MarJose <18107626+whoami213@users.noreply.github.com>
2021-09-28 12:35:52 +0800

    added expiration and length method

--- src/UrlShortener.php
+++ src/UrlShortener.php
@@ -2,20 +2,21 @@
 
 namespace Marjose\UrlShortener;
 
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Str;
 use Marjose\UrlShortener\Models\UrlShorten;
 
 class UrlShortener
 {
-    private $link;
-    private $new_link;
-    private $expiration;
-    private $length;
+    private string $link;
+    private int $expiration;
+    private int $length;
 
-//    public function __construct()
-//    {
-//
-//    }
+    public function __construct()
+    {
+        $this->expiration = config('UrlShortener.expiration');
+        $this->length = config('UrlShortener.length');
+    }
 
     /**
      * @throws \Exception
@@ -31,14 +32,37 @@ class UrlShortener
 
         return true;
     }
+    private function getData()
+    {
+        if ($this->link === null) {
+            return throw new \Exception('URL is missing or not valid');
+        }
+
+        return UrlShorten::where('shorten_link', $this->link)->first();
+    }
+
+    public function isExpired()
+    {
+        $data = $this->getData();
+        if (!isset($data)) {
+            return throw new \Exception('Entity not found');
+        }
+        return Carbon::parse($data->created_at)->addMinutes($this->expiration)->isPast();
+    }
+
+    public function setExpiration($minutes): UrlShortener
+    {
+        $this->expiration = $minutes;
+        return $this;
+    }
 
     public function setLength($x): UrlShortener
     {
         $this->length = $x;
-
         return $this;
     }
-    public function url($url)
+
+    public function url($url): UrlShortener
     {
         $this->link = $url;
         return $this;
@@ -53,12 +77,13 @@ class UrlShortener
             return throw new \Exception('URL is missing or not valid');
         }
         $string = Str::random($this->length);
-        $this->new_link = env('APP_URL')."/s/".$string;
+        $new_link = env('APP_URL')."/s/".$string;
 
         return UrlShorten::create([
         'param' => $string,
         'original_link' => $this->link,
-        'shorten_link' => $this->new_link,
+        'shorten_link' => $new_link,
         ]);
     }
+
 }
